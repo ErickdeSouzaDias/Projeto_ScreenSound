524d56a6b79d2094faffc4788976b34aa7020a60
Érick De Souza Dias <54341152+ErickdeSouzaDias@users.noreply.github.com>
2024-04-17 00:00:27 -0300

    V1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,61 @@
 ﻿// Screen Sound
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-Console.WriteLine(mensagemDeBoasVindas);
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine();
+    Console.WriteLine("1 - Para registrar uma banda");
+    Console.WriteLine("2 - Para mostrar bandas cadastradas");
+    Console.WriteLine("3 - Para avaliar uma banda");
+    Console.WriteLine("4 - Para exibir a média de uma banda");
+    Console.WriteLine("0 - Para sair da aplicação");
+
+    Console.WriteLine();
+    Console.Write("Digite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!; // O sinal de ! no final do comando diz que não aceito nulo
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: RegistrarBandas();
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break; 
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 0: Console.WriteLine("Tchau Tchau :)");
+            break;
+        default: Console.WriteLine("Opção inválida!");
+            break;
+    }
+}
+
+void RegistrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas");
+    Console.Write("Digite o nome da banda a ser registrada: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    Console.WriteLine($"Banda {nomeDaBanda} registrada com SUCESSO!!!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+
+ExibirOpcoesDoMenu();
--- obj/Debug/net7.0/PrimeiroProjeto.AssemblyInfo.cs
+++ obj/Debug/net7.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a9c18e576215ac46b8600bb84fadd350711469")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
